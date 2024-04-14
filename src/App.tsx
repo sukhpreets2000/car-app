0e51bfb30320a0d5fdec1c95b2bb63bb85004d27
sukhpreets2000 <sukhpreets2000@gmail.com>
2024-04-14 21:10:57 +0530

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ const App = () => {
   return (
     <>
       <h1 className="bg-lime-400 p-4">Hello Care care website</h1>
-      <p className="text-center my-3">This website is under development</p>
+      <p className="text-center my-3">This website is under development.</p>
     </>
   )
 }
