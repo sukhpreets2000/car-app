2beb1643778939feb2546878155771c6cda63bcb
sukhpreets2000 <sukhpreets2000@gmail.com>
2024-05-09 17:16:43 +0530

    add navbar component

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,51 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faClose } from '@fortawesome/free-solid-svg-icons';
+import logo from '/logo.png';
+import './Navbar.css';
+import { useState } from 'react';
+
+const Navbar = () => {
+    const [show, setShow] = useState(false);
+
+    const HamburgerHandler = () => {
+        setShow(!show);
+    }
+    return (
+        <>
+            <nav className="w-full bg-black text-yellow p-2 h-16">
+                <div className="container mx-auto flex items-center justify-between">
+                    <div>
+                        <a href="#" className="flex items-center gap-2">
+                            <img src={logo} alt="logo" className="w-12" />
+                            <p className="text-yellow">Car Care</p>
+                        </a>
+                    </div>
+                    <div>
+                        <ul className={`flex gap-4 ${show ? 'right-[0px]' : ''}`}>
+                            <li className="py-1">
+                                <a href="#">Home</a>
+                            </li>
+                            <li className="py-1">
+                                <a href="#">About</a>
+                            </li>
+                            <li className="py-1">
+                                <a href="#">Services</a>
+                            </li>
+                            <li className="py-1">
+                                <a href="#">Team</a>
+                            </li>
+                            <li className="py-1">
+                                <a href="#">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="bg-yellow text-black w-[30px] h-[30px] flex items-center justify-center rounded z-10 lg:hidden md:hidden sm:flex cursor-pointer" onClick={HamburgerHandler}>
+                        {show ? <FontAwesomeIcon icon={faClose} /> : <FontAwesomeIcon icon={faBars} />}
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      yellow: '#ffc107',
+      black: 'black',
+      blue: 'blue',
+    },
     extend: {},
   },
   plugins: [],
